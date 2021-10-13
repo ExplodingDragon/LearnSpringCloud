803348b34a47ea86c3501e23e50e75d303de03a3
Your <you@example.com>
2021-10-13 10:25:30 +0800

    集群 查看实例信息

--- 01-pay-provider-payment-8001/src/main/kotlin/i/learn/application.kt
+++ 01-pay-provider-payment-8001/src/main/kotlin/i/learn/application.kt
@@ -1,8 +1,10 @@
 package i.learn
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 class PayApplication
--- 01-pay-provider-payment-8001/src/main/kotlin/i/learn/controller/PayController.kt
+++ 01-pay-provider-payment-8001/src/main/kotlin/i/learn/controller/PayController.kt
@@ -3,6 +3,7 @@ package i.learn.controller
 import i.learn.entities.CommentResult
 import i.learn.entities.Payment
 import i.learn.service.PayService
+import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
-class PayController(private val payService: PayService) {
+class PayController(
+    private val payService: PayService,
+    private val discoveryClient: DiscoveryClient
+) {
     @PostMapping("/")
     fun create(@RequestBody payment: Payment): CommentResult<Boolean> {
         val res = payService.add(payment)
@@ -23,4 +27,10 @@ class PayController(private val payService: PayService) {
     fun get(@PathVariable id: Long): CommentResult<List<Payment>> {
         return CommentResult(0, "", payService.getPaymentById(id))
     }
+
+    @GetMapping("/info")
+    fun getInfo() = discoveryClient.services
+
+    @GetMapping("/info/{id}")
+    fun getInfo(id: String) = discoveryClient.getInstances(id)
 }
