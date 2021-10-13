aad8a94b7ed49c7ccf71adacedcb305b94667f19
Your <you@example.com>
2021-10-13 09:29:11 +0800

    eureka

--- 03-eureka-server/src/main/kotlin/i/learn/application.kt
+++ 03-eureka-server/src/main/kotlin/i/learn/application.kt
@@ -1,7 +1,9 @@
 package i.learn
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 class EurekaApplication
 
