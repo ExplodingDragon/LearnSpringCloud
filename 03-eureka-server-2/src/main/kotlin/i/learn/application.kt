e9bc691423242f3809597542e74e29461557de20
Your <you@example.com>
2021-10-13 09:55:18 +0800

    集群 eureka

---
+++ 03-eureka-server-2/src/main/kotlin/i/learn/application.kt
@@ -0,0 +1,12 @@
+package i.learn
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@EnableEurekaServer
+@SpringBootApplication
+class EurekaApplication
+
+fun main(args: Array<String>) {
+    runApplication<EurekaApplication>(*args)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,5 @@ rootProject.name = "SpringCloud"
 include("00-pay-api-commons")
 include("01-pay-provider-payment-8001")
 include("02-pay-provider-payment-8002")
-include("03-eureka-server")
+include("03-eureka-server-1")
+include("03-eureka-server-2")
